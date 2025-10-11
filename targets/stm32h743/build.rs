184ebf38be173571d976452703652bfc8342de4b
maxrt <max.r.tkachuk@gmail.com>
2025-10-11 03:20:19 +0300

    Make warning more visible

--- targets/stm32h743/build.rs
+++ targets/stm32h743/build.rs
@@ -1,6 +1,9 @@
 
+const ANSI_RED_FG: &str = "\x1b[31m";
+const ANSI_RESET_TEXT: &str = "\x1b[0m";
+
 fn main() {
-    println!("cargo::warning=stm32h743 is broken and wont run");
+    println!("cargo::warning={}! ! ! stm32h743 is broken and wont run{}", ANSI_RED_FG, ANSI_RESET_TEXT);
 
     println!("cargo:rustc-link-arg=-Tlink.x");
     println!("cargo:rustc-link-arg=-Ltargets/stm32h743/");
