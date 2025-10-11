cfcf7ab69aea78415a33d66ab566487c10a8be58
maxrt <max.r.tkachuk@gmail.com>
2025-10-11 03:08:03 +0300

    Fix linkage

--- targets/stm32h743/build.rs
+++ targets/stm32h743/build.rs
@@ -1,6 +1,6 @@
 
 fn main() {
     println!("cargo:rustc-link-arg=-Tlink.x");
-    println!("cargo:rustc-link-arg=-Lboards/stm32h743/");
+    println!("cargo:rustc-link-arg=-Ltargets/stm32h743/");
     println!("cargo:rustc-link-arg=--gc-sections");
 }
--- targets/stm32l051/build.rs
+++ targets/stm32l051/build.rs
@@ -1,6 +1,6 @@
 
 fn main() {
     println!("cargo:rustc-link-arg=-Tlink.x");
-    println!("cargo:rustc-link-arg=-Lboards/stm32l051/");
+    println!("cargo:rustc-link-arg=-Ltargets/stm32l051/");
     println!("cargo:rustc-link-arg=--gc-sections");
 }
