03e2965ec93c21568ce1c8032e9162d00c887436
maxrt <max.r.tkachuk@gmail.com>
2025-10-11 03:08:22 +0300

    Add warning about broken stm32h743 target

--- targets/stm32h743/build.rs
+++ targets/stm32h743/build.rs
@@ -1,5 +1,7 @@
 
 fn main() {
+    println!("cargo::warning=stm32h743 is broken and wont run");
+
     println!("cargo:rustc-link-arg=-Tlink.x");
     println!("cargo:rustc-link-arg=-Ltargets/stm32h743/");
     println!("cargo:rustc-link-arg=--gc-sections");
