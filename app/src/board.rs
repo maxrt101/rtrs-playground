7210393fbc994e86e71240a7698f0197de0afc8c
maxrt <max.r.tkachuk@gmail.com>
2025-10-18 05:26:28 +0300

    Add MicrosecondDelay & MicrosecondTickProvider callbacks

--- app/src/board.rs
+++ app/src/board.rs
@@ -1,56 +1,89 @@
-use rtrs::sync::{Mutex, RaceAction};
+use rtrs::sync::RwLock;
+use rtrs::time::TickProvider;
+
+use alloc::boxed::Box;
 
 pub enum CallbackType {
+    Systick(fn()),
+    TriggerCrash(fn()),
+    MicrosecondDelay(fn(u32)),
+    MicrosecondTickProvider(fn(&mut Box<dyn TickProvider<Tick = u32>>)),
+}
+
+pub enum Callback<'a> {
     Systick,
     TriggerCrash,
+    MicrosecondDelay(u32),
+    MicrosecondTickProvider(&'a mut Box<dyn TickProvider<Tick = u32>>),
 }
 
 struct Callbacks {
-    systick: Option<fn()>,
-    crash:   Option<fn()>,
+    systick:                   Option<fn()>,
+    crash:                     Option<fn()>,
+    microsecond_delay:         Option<fn(u32)>,
+    microsecond_tick_provider: Option<fn(&mut Box<dyn TickProvider<Tick = u32>>)>
 }
 
 impl Callbacks {
     const fn empty() -> Self {
         Self {
-            systick: None,
-            crash: None
+            systick:                   None,
+            crash:                     None,
+            microsecond_delay:         None,
+            microsecond_tick_provider: None
         }
     }
 }
 
-static CALLBACKS: Mutex<Callbacks> = Mutex::new(Callbacks::empty(), RaceAction::Crash);
+static CALLBACKS: RwLock<Callbacks> = RwLock::new(Callbacks::empty());
 
 pub struct BoardInterface {}
 
 impl BoardInterface {
-    pub fn register_callback(t: CallbackType, f: fn()) {
+    pub fn register_callback(cb: CallbackType) {
         let mut cbs = CALLBACKS.lock_mut();
 
-        match t {
-            CallbackType::Systick => {
+        match cb {
+            CallbackType::Systick(f) => {
                 (*cbs).systick = Some(f);
             }
-            CallbackType::TriggerCrash => {
+            CallbackType::TriggerCrash(f) => {
                 (*cbs).crash = Some(f);
+            }
+            CallbackType::MicrosecondDelay(f) => {
+                (*cbs).microsecond_delay = Some(f);
+            }
+            CallbackType::MicrosecondTickProvider(f) => {
+                (*cbs).microsecond_tick_provider = Some(f);
             }
         }
     }
 
-    pub fn callback(t: CallbackType) {
-        let cbs = CALLBACKS.lock_mut();
+    pub fn callback(cb: Callback) {
+        let cbs = CALLBACKS.lock();
 
-        match t {
-            CallbackType::Systick => {
+        match cb {
+            Callback::Systick => {
                 if let Some(f) = (*cbs).systick {
                     f();
                 }
             }
-            CallbackType::TriggerCrash => {
+            Callback::TriggerCrash => {
                 if let Some(f) = (*cbs).crash {
                     f();
                 }
             }
+            Callback::MicrosecondDelay(us) => {
+                if let Some(f) = (*cbs).microsecond_delay {
+                    f(us);
+                }
+            }
+            Callback::MicrosecondTickProvider(provider) => {
+                if let Some(f) = (*cbs).microsecond_tick_provider {
+                    f(provider);
+                }
+            }
         }
+
     }
 }
