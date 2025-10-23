3f6249309f3a58f49e8decfba3a5dc6cd58d3541
maxrt <max.r.tkachuk@gmail.com>
2025-10-23 22:17:34 +0300

    Remove unnecessary log meta

--- app/src/logs.rs
+++ app/src/logs.rs
@@ -4,9 +4,7 @@ use rtrs::object_insert;
 pub(crate) fn init_logs() {
     object_insert!(LOGGER_META_OBJECT_NAME,  ModuleMetaManager::new());
 
-    rtrs::log::register("main", Severity::Trace, 255);
-    rtrs::log::register("task", Severity::Trace, 255);
-
     rtrs::log::register("TEST", Severity::Trace, 255);
     rtrs::log::register("SHELL", Severity::Trace, 255);
+    rtrs::log::register("RADIO", Severity::Trace, 2);
 }
